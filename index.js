614fa406efbfd7833d5cec67d109f66a9c2e4bb9
Mento Mume <akpankevman@gmail.com>
2020-12-18 14:21:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const Joi = require('@hapi/joi');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+{id: 1, name: 'course1'},
+{id: 2, name: 'course2'},
+{id: 3, name: 'course3'}
+];
+app.get('/', (req,res) => {
+    res.send('hello world');
+});
+
+app.get('/api/courses', (req,res) =>{
+    res.send(courses);
+});
+
+app.post('/api/courses', (req,res)=> { 
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    const result = Joi.validate(req.body, schema);
+    if(result.error){
+        res.status(400).send(result.error);
+        return;     
+    }
+    const course = {
+        id:courses.length +1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.get('/api/courses/:id',(req,res)=>{
+   const course = courses.find(c => c.id === parseInt(req.params.id)); 
+   if(!course) res.status('404').send('the course with the given id was not found');
+   res.send(course);
+});
+//set port
+const port = process.env.PORT||3000;
+app.listen(port, ()=>console.log(`listening at port ${port}.....`));
